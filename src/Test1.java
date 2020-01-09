e00d335bca332451eac425e896f30c6ae806e3b7
V <V@192.168.1.212>
2020-01-09 14:40:34 +0530

    First commit...

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes") {
+			
+			System.out.println("Eat something...");
+		}
+		else {
+			
+			System.out.println("Do work...");
+		}
+	}
+
+}
